f6ae55355b7a4f499f3b65f12e195e1df374f8e6
depeng zhang <zhangdepeng3@gmail.com>
2025-07-24 09:13:59 +1000

    test: trigger coderabbit with dev config

--- src/MeetlyOmni.Api/Program.cs
+++ src/MeetlyOmni.Api/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 Console.WriteLine("This is a test for CodeRabbit");
+
 // Logging config (optional, but recommended)
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
